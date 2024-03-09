71e7cc32b08e7699505817a1cf7801fa2b09cf77
sanjay prajapat <sanjayprajapat68149@gmail.com>
2024-03-09 11:54:33 +0530

    file strudture created and RTK setup completed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>
+  <Provider store={store}>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
+  </Provider>
 );
 
---
+++ src/redux/combineReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from '@reduxjs/toolkit';
+// import dropdownSlice from '../features/folder/dropdownSlice';
+// import folderSlice from '../features/folder/folderSlice';
+// import fileSlice from '../features/file/fileSlice';
+// import authSlice from '../features/user/authSlice';
+
+const rootReducer = combineReducers({
+//   folders: folderSlice,
+//   auth: authSlice,
+//   folderDropdown: dropdownSlice,
+//   file: fileSlice,
+  // Add more reducers as needed
+});
+
+export default rootReducer;
